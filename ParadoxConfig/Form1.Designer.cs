40fc7e690abc864d70457eb9922e246712fc187b
Pete9xi <peter95rose@msn.com>
2023-07-18 14:50:14 +0100

    Add project files.

---
+++ ParadoxConfig/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace ParadoxConfig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxReleases = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 61);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Pack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxReleases
+            // 
+            this.comboBoxReleases.FormattingEnabled = true;
+            this.comboBoxReleases.Location = new System.Drawing.Point(193, 36);
+            this.comboBoxReleases.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxReleases.Name = "comboBoxReleases";
+            this.comboBoxReleases.Size = new System.Drawing.Size(141, 21);
+            this.comboBoxReleases.TabIndex = 1;
+            this.comboBoxReleases.SelectedIndexChanged += new System.EventHandler(this.comboBoxReleases_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a release to download";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter a password to be set in config.js\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 99);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(325, 127);
+            this.txtLog.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 238);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxReleases);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "ParadoxConfig";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxReleases;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ ParadoxConfig/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.IO.Compression;
+
+namespace ParadoxConfig
+{
+    public partial class Form1 : Form
+    {
+        private string extractedFilePath = "";
+        private string downloadedFileName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var selectedReleaseIndex = comboBoxReleases.SelectedIndex;
+            if (selectedReleaseIndex >= 0)
+            {
+                var selectedRelease = comboBoxReleases.SelectedItem;
+                var selectedSourceCodeZipUrl = $"https://github.com/Pete9xi/Paradox_AntiCheat/releases/download/{selectedRelease}/Paradox-AntiCheat-{selectedRelease}.mcpack";
+
+
+                txtLog.AppendText(selectedSourceCodeZipUrl + Environment.NewLine);
+                var downloadResult = await DownloadAndExtractFile(selectedSourceCodeZipUrl);
+                if (downloadResult)
+                {
+                    txtLog.AppendText("Starting to create the pack." + Environment.NewLine);
+                    createPack(textBox1.Text);
+                  
+
+                }
+                else
+                {
+                    txtLog.AppendText("Download or extraction failed." + Environment.NewLine);
+                }
+            }
+        }
+        private async Task<bool> DownloadAndExtractFile(string fileUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var fileName = Path.GetFileName(fileUrl);
+                    var saveFilePath = Path.Combine(Application.StartupPath, fileName);
+                    var fileBytes = await httpClient.GetByteArrayAsync(fileUrl);
+                    File.WriteAllBytes(saveFilePath, fileBytes);
+
+                    // Check if the file exists
+                    if (File.Exists(saveFilePath))
+                    {
+                        txtLog.AppendText("File downloaded successfully." + Environment.NewLine);
+
+                        // Rename the file extension from .mcpack to .zip
+                        var newFileName = Path.ChangeExtension(saveFilePath, ".zip");
+                        File.Move(saveFilePath, newFileName);
+                        var selectedVersion = comboBoxReleases.SelectedItem.ToString();
+                        var extractPath = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}/Paradox");
+                        Directory.CreateDirectory(extractPath);
+
+                        ZipFile.ExtractToDirectory(newFileName, extractPath);
+
+                      txtLog.AppendText("File extracted successfully." + Environment.NewLine);
+                        extractedFilePath = extractPath;
+                        downloadedFileName = saveFilePath;
+                        File.Delete(saveFilePath);
+                        File.Delete(newFileName);
+                        return true;
+                    }
+                    else
+                    {
+                        txtLog.AppendText("File download failed." + Environment.NewLine);
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    txtLog.AppendText($"An error occurred during file download and extraction: {ex.Message}" + Environment.NewLine);
+                    return false;
+                }
+            }
+        }
+
+
+        private void createPack(string password)
+        {
+            var selectedVersion = comboBoxReleases.SelectedItem.ToString();
+            var extractPath = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}/Paradox");
+            var outputFolderPath = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}");
+            var outputFilePath = Path.Combine(outputFolderPath, $"Paradox-AntiCheat-{ selectedVersion}.zip");
+
+            Directory.CreateDirectory(outputFolderPath);
+
+            // Update the password in the config.js file
+            var configFile = Path.Combine(extractPath, "scripts/data/config.js");
+            string[] lines = File.ReadAllLines(configFile);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("password: \"\""))
+                {
+                    lines[i] = lines[i].Replace("password: \"\"", $"password: \"{password}\"");
+                    break;
+                }
+            }
+            File.WriteAllLines(configFile, lines);
+
+            txtLog.AppendText("File updated successfully." + Environment.NewLine);
+
+            // Create the ZIP file with the desired folder structure
+            using (var zipArchive = ZipFile.Open(outputFilePath, ZipArchiveMode.Create))
+            {
+                var files = Directory.GetFiles(extractPath, "*", SearchOption.AllDirectories);
+                foreach (var filePath in files)
+                {
+                    var relativePath = Path.Combine("Paradox", filePath.Substring(extractPath.Length + 1));
+                    zipArchive.CreateEntryFromFile(filePath, relativePath, CompressionLevel.Optimal);
+                }
+            }
+
+            txtLog.AppendText("Files re-zipped successfully." + Environment.NewLine);
+            var saveFilePath = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}/Paradox-AntiCheat-{selectedVersion}.zip");
+            var newFileName = Path.ChangeExtension(saveFilePath, ".mcpack");
+            File.Move(saveFilePath, newFileName);
+            var finalePack = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}/Paradox-AntiCheat-{selectedVersion}.mcpack");
+            var applicationDir = Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{ selectedVersion}.mcpack");
+            File.Move(finalePack, applicationDir);
+            txtLog.AppendText("The pack has been created with the requested password can be imported." +Environment.NewLine);
+            Directory.Delete(Path.Combine(Application.StartupPath, $"Paradox-AntiCheat-{selectedVersion}"),true);
+        }
+        private void comboBoxReleases_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Paradox_AntiCheat", "1"));
+            var repo = "Pete9xi/Paradox_AntiCheat";
+            var releasesUrl = $"https://api.github.com/repos/{repo}/releases";
+            var releasesJson = await httpClient.GetStringAsync(releasesUrl);
+            var releases = JsonConvert.DeserializeObject<JArray>(releasesJson);
+            foreach (var release in releases)
+            {
+                var releaseName = (string)release["name"];
+                comboBoxReleases.Items.Add(releaseName);
+            }
+        }
+        
+
+
+
+    }
+}
+
